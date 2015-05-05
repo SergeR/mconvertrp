f1e2a4f8ba6d48fbdcf61a1cd75874c89e586dbe
Serge Rodovnichenko <sergerod@gmail.com>
2015-05-05 15:06:38 +0300

    Описание

---
+++ lib/config/plugin.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'name'        => 'Мультиконверт Почты России',
+    'description' => 'Доставка по России',
+    'icon'        => 'img/mconvertrp.png',
+    'version'     => '1.0.0',
+    'vendor'      => '670917',
+);
