762a675bb28dc634bc8166010c1748a1b73387b7
Serge Rodovnichenko <sergerod@gmail.com>
2015-05-06 17:03:09 +0300

    Описание и иконки

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -1,8 +1,9 @@
 <?php
 return array(
     'name'        => 'Мультиконверт Почты России',
-    'description' => 'Доставка по России',
-    'icon'        => 'img/mconvertrp.png',
+    'description' => 'Доставка по России почтовым отправлением Мультиконверт',
+    'icon'        => 'img/icon16.png',
+    'logo'        => 'img/multiconvert.png',
     'version'     => '1.0.0',
     'vendor'      => '670917',
 );
